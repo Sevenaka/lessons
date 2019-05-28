e96e0952c4a2030487291eb0aaa4613ab4fea651
Sevenaka <Sevenakaoxxy@gmail.com>
2019-05-28 23:08:25 +0600

    Lessons change 2

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+console.log(pi);
+
+let second = 2;
+console.log(second);
+
+//let answer = confirm("Are you here?");
+//console.log(answer)
+
+let answer = prompt("Есть ли вам 18?", "Да");
+console.log(typeof(null));
